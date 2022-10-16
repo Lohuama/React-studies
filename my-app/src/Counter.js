9eed67953a2608a8d170cc0b219303b7a780635d
Lohuama Lima <limalorruama98@gmail.com>
2022-10-16 20:46:06 -0300

    Hooks

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,57 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Counter extends React.Component{
-
-    //constructor é o primeiro ciclo de vida do codigo, pois é a primeira coisa q ele vai buscar para executar
-    constructor(props){
-        super(props);
-
-        this.state = {count: this.props.count}
-        this.add = this.add.bind(this)
+//HOOKS
+function Counter(props){
+    const [count, setCount] = useState(props.count)
+    function add(){
+        setCount(count + 1);
     }
 
-    add(){
-        //this.setState({ count: this.state.count + 1})
-        this.setState((state) => {return {count: state.count + 1}}, () => {
-            console.log(this.state)
-            localStorage.setItem("state", JSON.stringify(this.state))
-        })
+    return(
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={add}>add</button>
+        </div>
+    )
+}
+
+// class Counter extends React.Component{
+
+//     //constructor é o primeiro ciclo de vida do codigo, pois é a primeira coisa q ele vai buscar para executar
+//     constructor(props){
+//         super(props);
+
+//         this.state = {count: this.props.count}
+//         this.add = this.add.bind(this)
+//     }
+
+//     add(){
+//         //this.setState({ count: this.state.count + 1})
+//         this.setState((state) => {return {count: state.count + 1}}, () => {
+//             console.log(this.state)
+//             localStorage.setItem("state", JSON.stringify(this.state))
+//         })
         
-    }
+//     }
 
-    //segundo metodo do ciclo de vida - renderiza o metodo apos o estado
-    render(){
-        return(
-            <div>
-                <h1>Counter: {this.state.count} </h1>
-                <button onClick={this.add}>add</button>
-            </div>
-        )    
-    }
+//     //segundo metodo do ciclo de vida - renderiza o metodo apos o estado
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Counter: {this.state.count} </h1>
+//                 <button onClick={this.add}>add</button>
+//             </div>
+//         )    
+//     }
     
-    /*
-    EXEMPLOS DE METODOS DO CICLO DE VIDA DE UM COMPONENTE
+//     /*
+//     EXEMPLOS DE METODOS DO CICLO DE VIDA DE UM COMPONENTE
 
-    este ciclo informa se este metodo deve ser atualizado ou nao no componete
-     shouldComponentUpdate(){
-        return true;
-     }
+//     este ciclo informa se este metodo deve ser atualizado ou nao no componete
+//      shouldComponentUpdate(){
+//         return true;
+//      }
 
-     /* sera executdo dps que o componente for mostrado na tela, tbm é usado para fazer fetch
-    componentDidMount(){
-        this.setState(JSON.parse(localStorage.getItem('state')))
+//      /* sera executdo dps que o componente for mostrado na tela, tbm é usado para fazer fetch
+//     componentDidMount(){
+//         this.setState(JSON.parse(localStorage.getItem('state')))
 
-    }*/
+//     }*/
 
-    /* sera executado sempre que um componente for removido da tela 
+//     /* sera executado sempre que um componente for removido da tela 
     
-    componentWillUnmount(){
+//     componentWillUnmount(){
     
-   }
-     */
+//    }
+//      */
 
     
    
 
-}
+// }
 export default Counter
