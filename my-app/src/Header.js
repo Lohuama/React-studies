29fa06f16501f89eb34567aaf142baaa0dfa4c0a
Lohuama Lima <limalorruama98@gmail.com>
2022-10-16 18:55:57 -0300

    componentes com class

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -2,15 +2,22 @@ import React from 'react'
 import Menu from './Menu'
 import './App'
 
+class Header extends React.Component{
 
-function Header(props){
-    return (
-    <header className='header'>
-        <h2>{props.name}</h2>
-        <Menu links={props.links}></Menu>
-        
-    </header>
-  )
+    constructor(props){
+        super(props);
+    }
+
+    //METODO PAR RENDERIZAR O CONTEUDO
+    render(){return (
+        <header className='header'>
+            <h2>{this.props.name}</h2>
+            <Menu links={this.props.links}></Menu>
+            
+        </header>
+      )
+
+    }
 }
 
 export default Header
